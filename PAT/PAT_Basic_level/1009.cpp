187811fd1c1eac00e44fc187dbfbe44d7feea2b1
sphao <sphaodr@gmail.com>
2018-09-01 18:59:43 +0800

    1010

---
+++ PAT/PAT_Basic_level/1009.cpp
@@ -0,0 +1,47 @@
+/*
+* @Author: sphao
+* @Date:   2018-08-31 17:28:23
+* @Last Modified by:   sphao
+* @Last Modified time: 2018-08-31 18:22:24
+*/
+
+/*
+1009 说反话（20 分）
+给定一句英语，要求你编写程序，将句中所有单词的顺序颠倒输出。
+
+输入格式：
+测试输入包含一个测试用例，在一行内给出总长度不超过 80 的字符串。字符串由若干单词和若干空格组成，其中单词是由英文字母（大小写有区分）组成的字符串，单词之间用 1 个空格分开，输入保证句子末尾没有多余的空格。
+
+输出格式：
+每个测试用例的输出占一行，输出倒序后的句子。
+
+输入样例：
+Hello World Here I Come
+输出样例：
+Come I Here World Hello
+ */
+
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char const *argv[])
+{
+	char a[80];
+	//将输入存储进数组(scanf无法读入空格,所以使用getchar)
+	int i = 0;
+	while((a[i] = getchar())!='\n')
+	{
+		i++;
+	}
+	a[i] = '\0';	//输出时不输出换行符,以\0作替换
+	for(int j = i-1; j > 0; j--)
+	{
+		if(a[j] == ' ')
+		{
+			printf("%s ", &a[j+1]);
+			a[j] = '\0';
+		}
+	}
+	printf("%s", &a[0]);	//第一个输入前没有空格,循环中未能输出,直接补上即可
+	return 0;
+}
---
+++ PAT/PAT_Basic_level/1010.cpp
@@ -0,0 +1,55 @@
+/*
+* @Author: sphao
+* @Date:   2018-08-31 18:24:01
+* @Last Modified by:   sphao
+* @Last Modified time: 2018-09-01 18:58:02
+*/
+
+/*
+1010 一元多项式求导（25 分）
+设计函数求一元多项式的导数。
+
+输入格式:
+以指数递降方式输入多项式非零项系数和指数（绝对值均为不超过 1000 的整数）。数字间以空格分隔。
+
+输出格式:
+以与输入相同的格式输出导数多项式非零项的系数和指数。数字间以空格分隔，但结尾不能有多余空格。注意“零多项式”的指数和系数都是 0，但是表示为 0 0。
+
+输入样例:
+3 4 -5 2 6 1 -2 0
+输出样例:
+12 3 -10 1 6 0
+ */
+
+#include <cstdio>
+
+int main(int argc, char const *argv[])
+{
+	int a, n;
+	int f = 0;
+	do{
+		scanf("%d%d", &a, &n);
+		if(a ==0)
+			continue;
+		if(n != 0)
+		{
+			a = a*n;
+			n = n-1;
+			if(!(a == 0 && n == 0))
+			{
+				if(f == 0)
+				{
+					printf("%d %d", a, n);
+					f = 1;
+				}
+				else
+				{
+					printf(" %d %d", a, n);
+				}
+			}
+		}
+	}while(getchar() != '\n');
+	if(f == 0)
+		printf("0 0\n");
+	return 0;
+}
